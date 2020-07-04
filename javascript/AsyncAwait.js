16493e4533c319526b671cb28dad3a153578cf00
Ricardo Santos <rsanttos.dev@gmail.com>
2020-07-04 15:22:11 -0300

    Add files via upload

--- javascript/AsyncAwait.js
+++ javascript/AsyncAwait.js
@@ -1,1 +1,8 @@
-// Atualizando
+// Usando Async Await
+
+async function fetchList(url) {
+  const response = await fetch(url);
+  console.log(response);
+}
+
+fetchList('https://api.github.com/users/r-santtos');
